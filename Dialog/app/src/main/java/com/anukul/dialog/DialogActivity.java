36c84af1728bc2c414fafdf90466579a529266bf
anukul mehta <mehtaanukul@gmail.com>
2018-12-12 17:59:04 +0530

    Dialog update

--- Dialog/app/src/main/java/com/anukul/dialog/DialogActivity.java
+++ Dialog/app/src/main/java/com/anukul/dialog/DialogActivity.java
@@ -2,25 +2,38 @@ package com.anukul.dialog;
 
 import android.app.TimePickerDialog;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDialog;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.Calendar;
 
 
-public class DialogActivity extends AppCompatActivity implements View.OnClickListener {
+public class DialogActivity extends AppCompatActivity implements View.OnClickListener , SeekBar.OnSeekBarChangeListener {
 
     private Button alertBtn;
     private Button timeBtn;
     private Button customBtn;
     private Button colorBtn;
 
+    //colorPickerDialog
+    private SeekBar colorRedSk;
+    private SeekBar colorGreenSk;
+    private SeekBar colorBlueSk;
+    private SeekBar colorAlphaSk;
+    RelativeLayout colorPreview;
+    private int red = 0;
+    private int green = 0;
+    private int blue = 0;
+    private int alpha = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +41,9 @@ public class DialogActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_dialog);
 
         initView();
+
     }
+
 
     private void initView() {
         alertBtn = findViewById(R.id.activity_dialog_alertBtn);
@@ -40,6 +55,7 @@ public class DialogActivity extends AppCompatActivity implements View.OnClickLis
         timeBtn.setOnClickListener(this);
         customBtn.setOnClickListener(this);
         colorBtn.setOnClickListener(this);
+
     }
 
     @Override
@@ -63,8 +79,26 @@ public class DialogActivity extends AppCompatActivity implements View.OnClickLis
 
     private void openColorPickerDialog() {
 
+        final AppCompatDialog appCompatDialog = new AppCompatDialog(DialogActivity.this, R.style.Theme_AppCompat_Light_Dialog_MinWidth);
+
+        appCompatDialog.setContentView(R.layout.colorpicker_dialog);
+        appCompatDialog.setTitle("ColorPickerDialog");
+        appCompatDialog.show();
+
+        colorRedSk = appCompatDialog.findViewById(R.id.activity_colorpicker_redSeekbar);
+        colorGreenSk = appCompatDialog.findViewById(R.id.activity_colorpicker_greenSeekbar);
+        colorBlueSk = appCompatDialog.findViewById(R.id.activity_colorpicker_blueSeekbar);
+        colorAlphaSk = appCompatDialog.findViewById(R.id.activity_colorpicker_alphaSeekbar);
+        colorPreview = appCompatDialog.findViewById(R.id.activity_colorpicker_relativelayout);
+
+        colorRedSk.setOnSeekBarChangeListener(this);
+        colorGreenSk.setOnSeekBarChangeListener(this);
+        colorBlueSk.setOnSeekBarChangeListener(this);
+        colorAlphaSk.setOnSeekBarChangeListener(this);
+        
 
     }
+
 
     private void openCustomDialog() {
         final AppCompatDialog appCompatDialog = new AppCompatDialog(DialogActivity.this, R.style.Theme_AppCompat_Light_Dialog_MinWidth);
@@ -123,5 +157,39 @@ public class DialogActivity extends AppCompatActivity implements View.OnClickLis
             }
         });
         builder.show();
+    }
+
+    //ColorPickerDialog --> SeekBar Code
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        switch (seekBar.getId()){
+            case R.id.activity_colorpicker_redSeekbar:
+                Toast.makeText(this,"RED SEEKBAR",Toast.LENGTH_SHORT).show();
+                red = progress;
+                break;
+            case R.id.activity_colorpicker_greenSeekbar:
+                Toast.makeText(this,"GREEN SEEKBAR",Toast.LENGTH_SHORT).show();
+                green = progress;
+                break;
+            case R.id.activity_colorpicker_blueSeekbar:
+                Toast.makeText(this,"BLUE SEEKBAR",Toast.LENGTH_SHORT).show();
+                blue = progress;
+                break;
+            case R.id.activity_colorpicker_alphaSeekbar:
+                Toast.makeText(this,"ALPHA SEEKBAR",Toast.LENGTH_SHORT);
+                alpha = progress;
+                break;
+        }
+        colorPreview.setBackgroundColor(Color.argb(alpha,red,green,blue));
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
