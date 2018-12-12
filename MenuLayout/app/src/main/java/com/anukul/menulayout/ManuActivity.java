ebe1d671bac0a939f3206e345d2682ae3ee76f0a
anukul mehta <mehtaanukul@gmail.com>
2018-12-12 18:48:19 +0530

    Menu

---
+++ MenuLayout/app/src/main/java/com/anukul/menulayout/ManuActivity.java
@@ -0,0 +1,14 @@
+package com.anukul.menulayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ManuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manu);
+
+    }
+}
