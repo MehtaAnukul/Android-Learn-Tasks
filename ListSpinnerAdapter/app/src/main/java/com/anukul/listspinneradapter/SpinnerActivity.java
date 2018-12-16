dbbdedd0cc2f8a3050d20fe957646435b7e7d8f6
anukul mehta <mehtaanukul@gmail.com>
2018-12-16 12:33:10 +0530

    Listspinnerupdate

--- ListSpinnerAdapter/app/src/main/java/com/anukul/listspinneradapter/SpinnerActivity.java
+++ ListSpinnerAdapter/app/src/main/java/com/anukul/listspinneradapter/SpinnerActivity.java
@@ -8,23 +8,43 @@ import android.widget.Spinner;
 import java.util.ArrayList;
 
 public class SpinnerActivity extends AppCompatActivity {
-    private Spinner citySpinner;
+    private Spinner stateSpinner;
     ArrayAdapter<String> arrayAdapter;
-    ArrayList<String> stringCityArrayList;
+    ArrayList<String> stringStateArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spinner);
 
-        citySpinner = findViewById(R.id.activity_city_spinner);
+        stateSpinner = findViewById(R.id.activity_state_spinner);
 
-        stringCityArrayList = new ArrayList<>();
-        stringCityArrayList.add("Delhi");
-        stringCityArrayList.add("Mumbai");
+        stringStateArrayList = new ArrayList<>();
+        stringStateArrayList.add("Jammu & kashmir");
+        stringStateArrayList.add("Himanchal Pradesh");
+        stringStateArrayList.add("Uttarachal");
+        stringStateArrayList.add("Panjab");
+        stringStateArrayList.add("Haryana");
+        stringStateArrayList.add("Delhi");
+        stringStateArrayList.add("Rajesthan");
+        stringStateArrayList.add("Gujrat");
+        stringStateArrayList.add("Maharastra");
+        stringStateArrayList.add("Goa");
+        stringStateArrayList.add("Karnataka");
+        stringStateArrayList.add("Kerala");
+        stringStateArrayList.add("Tamilnadu");
+        stringStateArrayList.add("Pondicherry");
+        stringStateArrayList.add("Andhra Pradesh");
+        stringStateArrayList.add("Madhya Pradesh");
+        stringStateArrayList.add("Uttar Pradesh");
+        stringStateArrayList.add("Bihar");
+        stringStateArrayList.add("Jharkhand");
+        stringStateArrayList.add("Chhattisgath");
+        stringStateArrayList.add("Orrisa");
 
 
-        arrayAdapter = new ArrayAdapter<>(this,R.layout.city_name,R.id.city_name_Tv,stringCityArrayList);
-        citySpinner.setAdapter(arrayAdapter);
+
+        arrayAdapter = new ArrayAdapter<>(this,R.layout.stateofindia_name,R.id.stateofindia_name_Tv,stringStateArrayList);
+        stateSpinner.setAdapter(arrayAdapter);
     }
 }
