1edc6d2d9259329862d85ed9c78f71c303f8f118
anukul mehta <mehtaanukul@gmail.com>
2019-01-01 16:47:13 +0530

    update

--- IntentActivityForResultCalculator/app/src/main/java/com/anukul/intentactivityforresultcalculator/MainActivity.java
+++ IntentActivityForResultCalculator/app/src/main/java/com/anukul/intentactivityforresultcalculator/MainActivity.java
@@ -1,18 +1,22 @@
 package com.anukul.intentactivityforresultcalculator;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = MainActivity.class.getSimpleName();
     private Button sumBtn;
     private Button subBtn;
     private Button mulBtn;
     private Button divBtn;
     private TextView answerTv;
+    private int result;
+    private int returnCode;
 
     private Intent gotoSecondActivityIntent;
 
@@ -29,6 +33,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         subBtn = findViewById(R.id.activity_main_subBtn);
         mulBtn = findViewById(R.id.activity_main_mulBtn);
         divBtn = findViewById(R.id.activity_main_divBtn);
+        answerTv = findViewById(R.id.activity_main_answerTv);
 
         sumBtn.setOnClickListener(this);
         subBtn.setOnClickListener(this);
@@ -38,22 +43,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.activity_main_sumBtn:
-                gotoSecondActivityIntent = new Intent(MainActivity.this,SecondActivity.class);
-                startActivityForResult(gotoSecondActivityIntent,1);
+                gotoSecondActivityIntent = new Intent(MainActivity.this, SecondActivity.class);
+                gotoSecondActivityIntent.putExtra("KEY_CODE", 1);
+                startActivityForResult(gotoSecondActivityIntent, 100);
                 break;
             case R.id.activity_main_subBtn:
-                gotoSecondActivityIntent = new Intent(MainActivity.this,SecondActivity.class);
-                startActivityForResult(gotoSecondActivityIntent,2);
+                gotoSecondActivityIntent = new Intent(MainActivity.this, SecondActivity.class);
+                gotoSecondActivityIntent.putExtra("KEY_CODE", 2);
+                startActivityForResult(gotoSecondActivityIntent, 200);
                 break;
             case R.id.activity_main_mulBtn:
-                gotoSecondActivityIntent = new Intent(MainActivity.this,SecondActivity.class);
-                startActivityForResult(gotoSecondActivityIntent,3);
+                gotoSecondActivityIntent = new Intent(MainActivity.this, SecondActivity.class);
+                gotoSecondActivityIntent.putExtra("KEY_CODE", 3);
+                startActivityForResult(gotoSecondActivityIntent, 300);
                 break;
             case R.id.activity_main_divBtn:
-                gotoSecondActivityIntent = new Intent(MainActivity.this,SecondActivity.class);
-                startActivityForResult(gotoSecondActivityIntent,4);
+                gotoSecondActivityIntent = new Intent(MainActivity.this, SecondActivity.class);
+                gotoSecondActivityIntent.putExtra("KEY_CODE", 4);
+                startActivityForResult(gotoSecondActivityIntent, 400);
                 break;
         }
 
@@ -62,13 +71,58 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 1){
-            if(resultCode == RESULT_OK){
-                if(data != null){
-                    final int result = data.getIntExtra("ADD",10);
-                    answerTv.setText(result+"");
+        if (requestCode == 100) {
+            if (resultCode == RESULT_OK) {
+                if (data != null) {
+                    returnCode = data.getIntExtra("KEY_CODE_RETURN_ADD", 0);
+                    Log.e(TAG, "onActivityResult: " + returnCode);
+                    displayOutPut(returnCode, data);
                 }
             }
+        } else if (requestCode == 200) {
+            if (resultCode == RESULT_OK) {
+                if (data != null) {
+                    resultCode = data.getIntExtra("KEY_CODE_RETURN_SUB", 0);
+                    displayOutPut(resultCode, data);
+                }
+            }
+        } else if (requestCode == 300) {
+            if (resultCode == RESULT_OK) {
+                if (data != null) {
+                    resultCode = data.getIntExtra("KEY_CODE_RETURN_MUL", 0);
+                    displayOutPut(resultCode, data);
+                }
+            }
+        } else if (requestCode == 400) {
+            if (resultCode == RESULT_OK) {
+                if (data != null) {
+                    resultCode = data.getIntExtra("KEY_CODE_RETURN_DIV", 0);
+                    displayOutPut(resultCode, data);
+                }
+            }
+        }
+    }
+
+
+    private void displayOutPut(int returnCode, Intent data) {
+        switch (returnCode) {
+
+            case 1:
+                result = data.getIntExtra("ADD", 0);
+                answerTv.setText(result + "");
+                break;
+            case 2:
+                result = data.getIntExtra("SUB", 0);
+                answerTv.setText(result + "");
+                break;
+            case 3:
+                result = data.getIntExtra("MUL", 0);
+                answerTv.setText(result + "");
+                break;
+            case 4:
+                result = data.getIntExtra("DIV", 0);
+                answerTv.setText(result + "");
+                break;
         }
     }
 }
--- IntentActivityForResultCalculator/app/src/main/java/com/anukul/intentactivityforresultcalculator/SecondActivity.java
+++ IntentActivityForResultCalculator/app/src/main/java/com/anukul/intentactivityforresultcalculator/SecondActivity.java
@@ -1,8 +1,8 @@
 package com.anukul.intentactivityforresultcalculator;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,8 +14,11 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
     private Button cancelBtn;
     private int value1;
     private int value2;
+    private int operationCode;
+    private int ans;
 
     private Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +34,7 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
         cancelBtn = findViewById(R.id.activity_second_cencalBtn);
 
         intent = getIntent();
+        operationCode = intent.getIntExtra("KEY_CODE", 0);
 
         okBtn.setOnClickListener(this);
         cancelBtn.setOnClickListener(this);
@@ -42,25 +46,44 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
         value1 = Integer.parseInt(valueOneEd.getText().toString().trim());
         value2 = Integer.parseInt(valueSecEd.getText().toString().trim());
 
-        switch (v.getId()){
-
+        switch (v.getId()) {
             case R.id.activity_second_okBtn:
-                intent.putExtra("ADD",value1 + value2);
-                setResult(RESULT_OK,intent);
-                finish();
-            case 2:
-                intent.putExtra("SUB",value1 - value2);
-                setResult(RESULT_OK,intent);
-                finish();
-            case 3:
-                intent.putExtra("MUL",value1 * value2);
-                setResult(RESULT_OK,intent);
-                finish();
-            case 4:
-                intent.putExtra("MUL",value1 * value2);
-                setResult(RESULT_OK,intent);
-                finish();
+                performTask();
+                break;
         }
 
+    }
+
+    private void performTask() {
+        switch (operationCode) {
+            case 1:
+                ans = value1 + value2;
+                intent.putExtra("ADD", ans);
+                intent.putExtra("KEY_CODE_RETURN_ADD", 1);
+                setResult(RESULT_OK, intent);
+                finish();
+                break;
+            case 2:
+                ans = value1 - value2;
+                intent.putExtra("SUB", ans);
+                intent.putExtra("KEY_CODE_RETURN_SUB", 2);
+                setResult(RESULT_OK, intent);
+                finish();
+                break;
+            case 3:
+                ans = value1 * value2;
+                intent.putExtra("MUL", ans);
+                intent.putExtra("KEY_CODE_RETURN_MUL", 3);
+                setResult(RESULT_OK, intent);
+                finish();
+                break;
+            case 4:
+                ans = value1 / value2;
+                intent.putExtra("DIV", ans);
+                intent.putExtra("KEY_CODE_RETURN_DIV", 4);
+                setResult(RESULT_OK, intent);
+                finish();
+                break;
+        }
     }
 }
